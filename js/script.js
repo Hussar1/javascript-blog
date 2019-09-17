1af6b884038cf4e1d88fa4b2520443faa3805541
Przemek Drożniak <mr.shemeck@gmail.com>
2019-09-17 11:09:46 +0200

    ADD add .active functionality, for links, within titleClickHandler()

--- js/script.js
+++ js/script.js
@@ -8,17 +8,21 @@
 const titleClickHandler = function() {
     console.log('Link was clicked!');
     console.log(event)
+    const clickedElement = this;
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    console.log('clickedElement: ', clickedElement);
+    console.log('clickedElement (with plus): ' + clickedElement);
+    clickedElement.classList.add('active');
 
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .active');
 
     for(let activeArticle of activeArticles){
