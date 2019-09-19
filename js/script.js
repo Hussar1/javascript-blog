d7f0185a2256f99034da3db1eb22ab483de8d841
Przemek Drożniak <mr.shemeck@gmail.com>
2019-09-19 15:18:35 +0200

    ADD functionalities of module 6.4

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-'use strict';
+{'use strict';
 
 // document.getElementById('test-button').addEventListener('click', function(){
 //     const links = document.querySelectorAll('.titles a');
@@ -40,9 +40,55 @@ const titleClickHandler = function() {
     /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks() {
+
+    /* [DONE] remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    console.log('Removed contents of titleList.');
+
+
+
+    let html = '';
+
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+    console.log('articles: ', articles);
+    
+    for(let article of articles) {    
+
+        /* get the article id */
+        const articleId = article.getAttribute('id');
+        console.log('articleId: ', articleId);
+
+        /* find the title element */
+
+        /* get the title from the title element */
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+        console.log('articleTitle: ', articleTitle);
+
+        /* create HTML of the link */
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+        console.log('linkHTML: ', linkHTML);
+        /* insert link into titleList */
+        html = html + linkHTML;
+        console.log('html: ', html);
+    }
+    titleList.innerHTML = html;
+
+    const links = document.querySelectorAll('.titles a');
+    console.log('links: ', links)
   
-const links = document.querySelectorAll('.titles a');
-  
-for(let link of links) {
+    for(let link of links) {
     link.addEventListener('click', titleClickHandler);
+    }
+}
+
+generateTitleLinks();
+
 }
