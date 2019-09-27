51afdcc2982a9bd195980663a9ac38aeacb37669
Przemek Drożniak <mr.shemeck@gmail.com>
2019-09-27 12:06:36 +0200

    ADD display all tags in the right-hand sidebar functionality within generateTags()

--- js/script.js
+++ js/script.js
@@ -76,6 +76,8 @@
 
 
   function generateTags(){
+    /* [NEW] create a new variable allTags with an empty array */
+    let allTags = [];
     /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
     // console.log('generateTags() articles: ', articles);
@@ -99,13 +101,21 @@
         // console.log('linkHTML: ', linkHTML);
         /* add generated code to html variable */
         html = html + linkHTML;
+        /* [NEW] check if this link is NOT already in allTags */
+        if(allTags.indexOf(linkHTML) == -1) {
+          /* [NEW] add generated code to allTags array */
+          allTags.push(linkHTML);
+        }
         /* END LOOP: for each tag */
       }
-
       /* insert HTML of all the links into the tags wrapper */
       tagsWrapper.innerHTML = html;
       /* END LOOP: for every article: */
     }
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector('.tags');
+    /* [NEW] add html from allTags to tagList */
+    tagList.innerHTML = allTags.join(' ');
   }
 
   generateTags();
